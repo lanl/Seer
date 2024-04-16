583e966f80dcb33570f0e3e9360c4a5d643ef6f7
Pascal Grosset <pascalgrosset@lanl.gov>
2024-04-16 15:31:34 -0600

    cleaned up

--- evn/env_darwin_jupyter.sh
+++
@@ -1,3 +0,0 @@
-# Environment for Jupyter notebook
-module purge 
-spack load -r /4m7jqu6 #py-sdskv
--- evn/env_darwin_mochiServer.sh
+++
@@ -1,5 +0,0 @@
-# Environment for Mochi server
-module purge
-
-spack load -r /ytwvrre # margo
-spack load -r /xvkjxkz #sdskeyval
